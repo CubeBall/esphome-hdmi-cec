dd5a669f2599ca9dbaf12cf481eecce74d008a37
CubeBall <foxtrot71@mail.ru>
2022-05-24 18:09:35 +0300

    Initial Playback and CEC repo

--- components/hdmi_cec/__init__.py
+++ components/hdmi_cec/__init__.py
@@ -107,7 +107,8 @@ async def to_code(config):
     cg.add_library(
         None,
         None,
-        "https://github.com/johnboiles/CEC.git#d4b642c90f48f4e46c2cb8c234ab6dd2e5aee7c5",
+        # "https://github.com/johnboiles/CEC.git#d4b642c90f48f4e46c2cb8c234ab6dd2e5aee7c5",
+        "https://github.com/CubeBall/CEC.git",
     )
     cg.add(var.set_address([config[CONF_ADDRESS]]))
     cg.add(var.set_physical_address([config[CONF_PHYSICAL_ADDRESS]]))
